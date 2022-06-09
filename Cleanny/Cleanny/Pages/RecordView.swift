4d39b1a1dac8847f3ec2cdfb9e49ee799f45564e
mycookie1 <103025266+mycookie1@users.noreply.github.com>
2022-06-10 08:34:10 +0900

    ChartView
    added ChartView

--- Cleanny/Cleanny/Pages/RecordView.swift
+++ Cleanny/Cleanny/Pages/RecordView.swift
@@ -9,31 +9,10 @@
 import SwiftUI
 
 struct RecordView: View {
-    @Environment(\.calendar) var calendar
-     
-    private var year: DateInterval {
-          calendar.dateInterval(of: .month, for: Date())!
-      }
     
     var body: some View {
-      
-        
-          VStack{
-            CalendarView(interval: self.year) { date in
-                    Text("30")
-                        .hidden()
-                        .padding(8)
-                        .background(1 != 1 ? Color.red : Color.blue) // Make your logic
-                        .clipShape(Rectangle())
-                        .cornerRadius(4)
-                        .padding(4)
-                        .overlay(
-                            Text(String(self.calendar.component(.day, from: date)))
-                                .foregroundColor(Color.black)
-                                .underline(2 == 2) //Make your own logic
-                        )
-            }
-            Spacer()
+        VStack{
+            ChartView()
         }
     }
 }
