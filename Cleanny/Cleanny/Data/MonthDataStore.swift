d862ce21259fe65a9d01c552f83afcc0212369d6
jeohong <jeohong59@gmail.com>
2022-06-14 17:22:00 +0900

    feat: CharView 에서 청소 종류 한국어로 수정

--- Cleanny/Cleanny/Data/MonthDataStore.swift
+++ Cleanny/Cleanny/Data/MonthDataStore.swift
@@ -10,23 +10,33 @@ import Foundation
 
 class MonthDataStore: ObservableObject {
     @Published var list: [Month]
-   
+    @Published var listKo: [Month]
+    
     init() {
-        list = [Month(name: "DisposeTrash"),
-                Month(name: "Laundary"),
-                Month(name: "ToiletCleaning"),
-                Month(name: "FloorCleaning"),
-                Month(name: "DishWashing"),
-                Month(name: "TidyUp")
-           
+        list = [
+            Month(name: "DisposeTrash"),
+            Month(name: "Laundary"),
+            Month(name: "ToiletCleaning"),
+            Month(name: "FloorCleaning"),
+            Month(name: "DishWashing"),
+            Month(name: "TidyUp")
+        ]
+        
+        listKo = [
+            Month(name: "분리수거"),
+            Month(name: "세탁"),
+            Month(name: "욕실청소"),
+            Month(name: "바닥청소"),
+            Month(name: "설거지"),
+            Month(name: "정리정돈")
         ]
     }
     
     func addCnt() {
-//        guard let cleaning = cleaning else {
-//            return
-//        }
+        //        guard let cleaning = cleaning else {
+        //            return
+        //        }
         
-
+        
     }
 }
